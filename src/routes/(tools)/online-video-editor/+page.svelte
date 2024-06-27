ef0cc00134d65ec218269569ef7ed9241d422340
yadavsk098 <yadavsk098@gmail.com>
2024-06-27 20:28:52 +0530

    changed page file text

--- src/routes/(tools)/online-video-editor/+page.svelte
+++ src/routes/(tools)/online-video-editor/+page.svelte
@@ -1,9 +1,10 @@
 <script>
-
+// let a = 10;
 </script>
 
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
 	<!-- Add tool here -->
+	Hello
 </div>
 
 <style>
