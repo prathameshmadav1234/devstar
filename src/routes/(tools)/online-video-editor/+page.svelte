0b2b5b22ad4ae9d075c49d90544282dc8b40b200
yadavsk098 <yadavsk098@gmail.com>
2024-06-27 20:46:13 +0530

    added h1

--- src/routes/(tools)/online-video-editor/+page.svelte
+++ src/routes/(tools)/online-video-editor/+page.svelte
@@ -5,6 +5,7 @@
 <div class="card gap-16 items-center mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 overflow-hidden rounded-lg">
 	<!-- Add tool here -->
 	Hello
+	<h1>sample text</h1>
 </div>
 
 <style>
