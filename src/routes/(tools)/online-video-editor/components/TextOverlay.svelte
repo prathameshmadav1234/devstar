46a5511ded1bc1014494358f27d2890c4872d638
prathameshmadav1234 <119469755+prathameshmadav1234@users.noreply.github.com>
2024-06-28 18:30:37 +0530

    components
    h

---
+++ src/routes/(tools)/online-video-editor/components/TextOverlay.svelte
@@ -0,0 +1,37 @@
+<script>
+    export let overlayText;
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function applyOverlay() {
+        dispatch('apply');
+    }
+</script>
+
+<div>
+    <label>
+        Overlay Text:
+        <input type="text" bind:value={overlayText} />
+    </label>
+    <button on:click={applyOverlay}>Apply Text Overlay</button>
+</div>
+
+<style>
+    div {
+        margin-top: 10px;
+    }
+    label {
+        display: block;
+        margin: 5px 0;
+    }
+    button {
+        margin: 5px 0;
+    }
+</style>
+
+
+
+
+
+<!-- Saurabh -->
